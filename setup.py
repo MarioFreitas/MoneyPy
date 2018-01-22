92cb50d0c826cd7be925203e63456d5f6c81419d
Mario Freitas <mariofreitas.enc@gmail.com>
2018-01-22 11:39:14 -0200

    New Icon

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 # from setuptools import setup
 from distutils.core import setup
 
-setup(name='ProjectName',
-      version='0.1',
-      description='Short Description',
-      url='https://github.com/UserName/Repo',
-      author='Mr. Author',
-      author_email='my.email@gmail.com',
+setup(name='MoneyPy',
+      version='1.0',
+      description='Financial Control Assistant',
+      url='https://github.com/MarioRaul/MoneyPy',
+      author='Mario Raul Freitas',
+      author_email='mariofreitas.enc@gmail.com',
       license='MIT',
       packages=[],
       install_requires=[
           'numpy',
-          'matplotlib'],
+          'matplotlib',
+          'openpyxl'],
       zip_safe=False)
